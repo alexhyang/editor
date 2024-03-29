35caa32a0df07d168904a47f3a40ae93cfbdd86f
Alex Yang <alex.hyang.dev@gmail.com>
2024-03-29 16:44:01 -0700

    update Main to launch GUI

--- src/main/ui/Main.java
+++ src/main/ui/Main.java
@@ -3,8 +3,9 @@ package ui;
 public class Main {
     // code of this method is based on FitLifeGymKiosk project
     public static void main(String[] args) {
-        Terminal terminal = new Terminal();
-        terminal.start();
-        terminal.endProgram();
+        // Terminal terminal = new Terminal();
+        // terminal.start();
+        // terminal.endProgram();
+        new AppUI();
     }
 }
